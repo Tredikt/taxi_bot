8fd2ef421dc141ca8b16f791c2aeba3681cbee5d
Tredikt <ivantkacev7810@gmail.com>
2024-02-26 12:50:19 +0300

    table users

--- utils/db_api/database.py
+++ utils/db_api/database.py
@@ -3,4 +3,17 @@ from sqlite3 import connect
 
 class DataBase:
     def __init__(self, db_name):
-        self.db = connect(database=db_name)
+        self.conn = connect(database=db_name)
+        self.cur = self.conn.cursor()
+
+        self.cur.execute(
+            """
+            CREATE TABLE IF NOT EXISTS users(
+                tg_id INTEGER PRIMARY KEY,
+                username TEXT,
+                fullname TEXT
+            )
+            """
+        )
+
+        self.conn.commit()
